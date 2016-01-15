6e7a2b746453449fc63bbd4450c1ee675c76048d
Sebastian Thiele <sebastian.thiele@infopark.de>
2016-01-15 16:48:33 +0100

    new readme, code optimization, fzz

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # green-thumb
 Arduino project for a batter flower live
+
+## Components
+* *Arduino Uno Board*
+* *Temperature Sensor* - Like TMP36 or DS18S20; `–55 ‑ +125 °C`
+
+## Breadboard Design
+
+![Breadboard Design](https://raw.githubusercontent.com/sethiele/green-thumb/dev/fzz/green-thumb.png)
+
+
+## Version
+0.0.1 - Temperature
--- src/main.ino
+++ src/main.ino
@@ -1,19 +1,19 @@
-#define LEDPIN        13
-#define TEMP_PIN_OUT  7
-#define TEMP_PIN_IN   A0
+const int READING_PIN =   13;     // LED to display metering
+const int TEMP_PIN_OUT =  7;      // Pin for VCC to the thermometer
+                                  // For Power Save it should not be online
+                                  // all the time
+const int TEMP_PIN_IN =   A0;     // Input of thermometer
 
 void setup(){
   Serial.begin(9600);
-  pinMode(LEDPIN, OUTPUT);
+  pinMode(READING_PIN, OUTPUT);
   pinMode(TEMP_PIN_OUT, OUTPUT);
   digitalWrite(TEMP_PIN_OUT, HIGH);
 }
 
 void loop() {
-  digitalWrite(LEDPIN, HIGH);
-  float temperatur_val = read_temp();
+  float temperatur_val = read_temperature();
   int temperatur = map(temperatur_val, 0, 410, -50, 150);
-  digitalWrite(LEDPIN, LOW);
   Serial.print(temperatur);
   Serial.print("C - ");
   Serial.print(temperatur_val);
@@ -21,11 +21,60 @@ void loop() {
   delay(2000);
 }
 
-int read_temp(){
-  int temp = 0;
+/**
+ * Read temperature from the thermometer 10 times and return the everage
+ * @return {float} the everage themperature of 10 reads
+ */
+float read_temperature(){
+  display_read(TEMP_PIN_IN);
+  digitalWrite(TEMP_PIN_OUT, HIGH); // start the thermometer
+  delay(500);                       // wait 0.5 sek for warmup
+  float temp = 0;
   for(int i = 0; i < 10; i++){
-    temp += analogRead(A0);
-    delay(100);
+    delay(500);                     // wait for 0.5 sek
+    temp += analogRead(TEMP_PIN_IN);// read the temperature
   }
-  return temp/10;
+  digitalWrite(TEMP_PIN_OUT, LOW);  // stop the thermometer
+  return temp/10;                   // return the everage temperature
+  display_read_ready(TEMP_PIN_IN);
+}
+
+/**
+ * display mattering is in process
+ * @param  {int} int what          What is processed (Pin Constant)
+ * @return {void}
+ */
+void display_read(int what){
+  digitalWrite(READING_PIN, HIGH);
+  Serial.print("Start reading ");
+  Serial.println(identify_element(what));
+}
+
+/**
+ * display mattering is ready
+ * @param  {int} int what          What is processed (Pin Constant)
+ * @return {void}
+ */
+void display_read_ready(int what){
+  Serial.print("Finished reading ");
+  Serial.println(identify_element(what));
+  digitalWrite(READING_PIN, LOW);
+}
+
+/**
+ * Identify what is in process
+ * @param  {int} int what          Pin Constant
+ * @return {String}     Name of Element
+ */
+String identify_element(int what){
+  String readElement;
+  switch (what){
+    case TEMP_PIN_IN:
+      readElement = "Temperature";
+      break;
+    default:
+      readElement = String(what);
+      break;
+  }
+  return readElement;
 }
