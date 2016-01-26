3e2aa54af48631c2d3dfd952e42c8cc056fe2c65
Sebastian Thiele <sebastian.thiele@infopark.de>
2016-01-26 20:21:07 +0100

    moisture only

--- green-thumb-lite/green-thumb-lite.ino
+++ green-thumb-lite/green-thumb-lite.ino
@@ -1,9 +1,22 @@
+int StatusPin = 13;
+int MousturePin = A0;
+int Mousture = 0;
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(StatusPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Mousture = analogRead(MousturePin);
+  Serial.println(Mousture);
+  
+  if (Mousture < 370){
+    digitalWrite(StatusPin, HIGH); // to wet
+  } else if (Mousture >=370 && Mousture < 600) {
+    digitalWrite(StatusPin, LOW); // perfect
+  } else {
+    digitalWrite(StatusPin, HIGH); // to dry
+  }
 
+  delay(60 * 1000);
 }
